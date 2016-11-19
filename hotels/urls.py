677982440869f1f0ad3bd2108e902db44abf6659
FinleyCoding <fengli616@gmail.com>
2016-11-19 17:01:02 +0800

    complete feature: hotel_list

--- hotels/urls.py
+++ hotels/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<city_name>[0-9a-z]+)/$', views.index, name='index'),
-
+    url(r'^(?P<city>[0-9a-z]+)/$', views.hotel_list, name='hotel_list'),
 ]
--- hotels/views.py
+++ hotels/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Hotel
+
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, world")
+def hotel_list(request, city):
+
+    hotels = Hotel.objects.filter(city=city)
+    context = {
+        'hotels': hotels,
+    }
+    return render(request, 'hotels/hotel_list.html', context)
