a4c8103b3d466ba3103d3906101944412e68e1bf
Li Feng <fengli616@gmail.com>
2017-03-12 23:54:39 -0700

    implement seed method and add this step to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,15 @@
 
 /usr/local/bin/wait_for_it.sh $POSTGRES_HOST:$POSTGRES_PORT -t 60
 
+echo 'Migrating database changes.'
 python manage.py migrate
+
+echo 'Creating super user.'
+echo "from django.contrib.auth.models import User; \
+User.objects.create_superuser('admin', 'admin@admin.com', \
+'${SUPERUSER_PASSWORD}')" | python manage.py shell 2> /dev/null
+
+echo 'Generating data to database.'
+python manage.py seed
 
 gunicorn trazip.wsgi -b 0.0.0.0:80 --workers 3 --log-level DEBUG
