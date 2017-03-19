91e24db2f8b10712b43e1bfd11c8f240c8cb203a
Li Feng <fengli616@gmail.com>
2017-03-18 21:09:49 -0700

    Add default logger "trazip" to website.

--- frontier/views/home.py
+++ frontier/views/home.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import Http404
@@ -7,10 +9,12 @@ from hotel.models.hotel import Hotel
 from hotel.models.quotedprice import QuotedPrice
 from hotel.models.room import Room
 
+logger = logging.getLogger('trazip')
 
 def home(request):
     try:
         city_1 = City.objects.get(pk=1)
+        logger.debug('Getting city object {}'.format(repr(city_1)))
     except City.DoesNotExist:
         raise Http404("Cannot find city where pk=1.")
     context = {
